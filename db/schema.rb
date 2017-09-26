6b4ce8176799bbb870242b257720449bed9ccc7b
AntonioPimentel <antonio.pimentel@usach.cl>
2017-09-25 22:35:45 -0300

    ad

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
- 
 
 ActiveRecord::Schema.define(version: 20170920184856) do
 
@@ -220,5 +219,4 @@ ActiveRecord::Schema.define(version: 20170920184856) do
   add_foreign_key "users", "cities"
   add_foreign_key "users", "disc_profiles"
   add_foreign_key "users", "universities"
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#p = Region.create!(nombre: "Metropolitana")
+
+City.create!({nombre: "Lampa", region: "Metropolitana"}, {nombre: "Til Til",
+region: "Metropolitana"}, {nombre: "Pirque", region: "Metropolitana"}, {nombre:
+"Puente Alto", region: "Metropolitana"}, {nombre: "San José de Maipo", region: "Metropolitana"},
+{nombre: "Buin", region: "Metropolitana"},{nombre: "Calera de Tango", region:
+"Metropolitana"}, {nombre: "Paine", region: "Metropolitana"}, {nombre:
+"San Bernardo", region: "Metropolitana"}, {nombre: "Alhué", region: "Metropolitana"},
+{nombre: "Curacaví", region: "Metropolitana"}, {nombre: "María Pinto", region:
+"Metropolitana"}, {nombre: "Melipilla", region: "Metropolitana"}, {nombre:
+"San Pedro", region: "Metropolitana"}, {nombre: "Cerrillos", region: "Metropolitana"},
+{nombre: "Cerro Navia", region: "Metropolitana"}, {nombre: "Conchalí", region:
+"Metropolitana"}, {nombre: "El Bosque", region:
+"Metropolitana"}, {nombre: "Conchalí", region:
+"Metropolitana"})
