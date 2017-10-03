d0fb73f34be848654a80ac7df2ec5e0819f1162a
Matias Jara <mjara.figueroa@gmail.com>
2017-10-03 15:29:41 -0300

    region y ciudad prueba

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-a = 1
-20.times do
-  Publication.create(titulo: "algo #{a}",
-    limite: Time.zone.now,
-    type_id: 1,
-    problema: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    area_id: 1,
-    description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    premios: 'premios muchos',
-    company_id: 1
-  )
-  a += 1
-end
+r = Region.create(nombre: 'los lagos')
+c = City.create(nombre: 'osorno',region: c)
+
+# a = 1
+# 20.times do
+#   Publication.create(titulo: "algo #{a}",
+#     limite: Time.zone.now,
+#     type_id: 1,
+#     problema: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+#     area_id: 1,
+#     description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+#     premios: 'premios muchos',
+#     company_id: 1
+#   )
+#   a += 1
+# end
