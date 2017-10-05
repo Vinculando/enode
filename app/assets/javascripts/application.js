d8e56cfb7a691987d2300dd8618f5f08e7efcb6c
Matias Jara <mjara.figueroa@gmail.com>
2017-10-05 11:20:59 -0300

    borrar basura, agregar routes after destroy user session

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,21 +9,13 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require Chart.bundle
-//= require chartkick
-//= require Chart
-//= require jquery
-//= require jquery-1.8.3.min
+//
 //= require bootstrap.min
 //= require bootstrap-select
 //= require jquery.dcjqaccordion.2.7
 //= require jquery.scrollTo.min
 //= require jquery.nicescroll
-//= require jquery.sparkline
 //= require common-scripts
-//= require jquery.gritter
-//= require gritter-conf
-//= require sparkline-chart
 //= require moment
 //= require moment/es.js
 //= require bootstrap-datetimepicker
