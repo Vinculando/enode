43ef0c007033f23cb5d60e3dfc552d7dcc2f1a34
AntonioPimentel <antonio.pimentel@usach.cl>
2017-10-06 16:57:41 -0300

    Mailing funcionando, se uso rama rollbar

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,21 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.perform_caching = false
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+config.action_mailer.delivery_method = :smtp 
+config.action_mailer.perform_deliveries = true
+config.action_mailer.perform_caching = false
+config.action_mailer.raise_delivery_errors = true 
+config.action_mailer.default :charset => "utf-8"
+
+ActionMailer::Base.smtp_settings = {
+  :address => "shaka.dhn.cl",
+  :port => 26,
+  :authentication => :login, 
+  :domain => 'enode.cl',
+  :user_name => ENV['email_user'],
+  :password => ENV['email_password']
+   }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
