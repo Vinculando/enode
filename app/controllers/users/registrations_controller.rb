c9dbce6d94cc4bf0b2fb0e585cb3133b8119725b
AntonioPimentel <antonio.pimentel@usach.cl>
2017-10-10 16:26:19 -0300

    Dropdown Region/ciudad anidado con js

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,14 +3,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
    before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
+   def new
+    @regions = Region.all
+    @cities = City.all 
+    super
+   end
+
+
+  # def create
+  #  super
   # end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+
 
   # GET /resource/edit
   # def edit
@@ -40,7 +44,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:rut, :nombre, :paterno, :materno, :fechanac, :genero, :city_id])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:rut, :nombre, :paterno, :materno, :fechanac, :genero, :city_id, :region_id])
    end
 
   # If you have extra params to permit, append them to the sanitizer.
